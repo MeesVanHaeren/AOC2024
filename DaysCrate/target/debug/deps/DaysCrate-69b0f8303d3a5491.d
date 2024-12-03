3d153fc3f9412751e259e7f7da0d64fc4c86b7d2
Mees <m.vanhaeren@student.han.nl>
2024-12-03 14:32:39 +0100

    exclude target

--- DaysCrate/target/debug/deps/DaysCrate-69b0f8303d3a5491.d
+++
@@ -1,7 +0,0 @@
-/home/ya-boi-mees/Documents/aoc2024/DaysCrate/target/debug/deps/DaysCrate-69b0f8303d3a5491: src/main.rs src/day1.rs src/day2.rs
-
-/home/ya-boi-mees/Documents/aoc2024/DaysCrate/target/debug/deps/DaysCrate-69b0f8303d3a5491.d: src/main.rs src/day1.rs src/day2.rs
-
-src/main.rs:
-src/day1.rs:
-src/day2.rs:
--- DaysCrate/target/debug/deps/DaysCrate-7f31454e27ab0153.d
+++
@@ -1,7 +0,0 @@
-/home/nellylorey/Documents/frivolous coding/aoc2024/AOC2024/DaysCrate/target/debug/deps/DaysCrate-7f31454e27ab0153: src/main.rs src/day1.rs src/day2.rs
-
-/home/nellylorey/Documents/frivolous coding/aoc2024/AOC2024/DaysCrate/target/debug/deps/DaysCrate-7f31454e27ab0153.d: src/main.rs src/day1.rs src/day2.rs
-
-src/main.rs:
-src/day1.rs:
-src/day2.rs:
