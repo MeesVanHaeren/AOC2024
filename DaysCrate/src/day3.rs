6396947f3279790975450bc77ff52993f5e758bb
Mees <m.vanhaeren@student.han.nl>
2024-12-03 14:29:26 +0100

    day 3 complete!

--- DaysCrate/src/day3.rs
+++ DaysCrate/src/day3.rs
@@ -8,11 +8,49 @@ pub fn day3() {
     let fileString = fs::read_to_string(FILEPATH).expect("Could not read file");
     //filter out all the mul operation with regex
     // mul\(\d+,\d+\)
-    let muls = getMuls(fileString);
+    let muls = getMuls(&fileString);
     //parse the mul operations into a pair of numbers
     let sum = getTotalFromMuls(muls);
     //interperet and sum up numbers
     println!("The sum of all muls: {}", sum);
+
+    //filter out all the valid operations with regex
+    // mul\(\d+,\d+\)|do\(\)|don't\(\)
+    let operations = getOperations(&fileString);
+    //filter out all the mul operations that don't matter
+    let realMuls = filter(operations);
+    //get the real sum
+    let realSum = getTotalFromMuls(realMuls);
+    println!("The sum of all valid muls: {}", realSum);
+
+}
+
+fn filter(operations: Vec<String>) -> Vec<String> {
+    let mut output: Vec<String> = Vec::new();
+    let mut validity = true;
+
+    for operation in operations {
+        if operation == "do()" {
+            validity = true;
+        } else if operation == "don't()"{
+            validity = false;
+        } else {
+            if validity {
+                output.push(operation);
+            }
+        }
+    }
+
+    output
+}
+
+fn getOperations(input: &String) -> Vec<String> {
+    let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
+    let mut output : Vec<String> = Vec::new();
+    for match_ in re.captures_iter(input.as_str()) {
+        output.push(match_[0].parse().unwrap());
+    }
+    output
 }
 
 fn getTotalFromMuls(muls: Vec<String>) -> i64 {
@@ -30,7 +68,7 @@ fn getTotalFromMuls(muls: Vec<String>) -> i64 {
     total
 }
 
-fn getMuls(inputString: String) -> Vec<String> {
+fn getMuls(inputString: &String) -> Vec<String> {
     let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let mut output : Vec<String> = Vec::new();
     for match_ in re.captures_iter(inputString.as_str()) {
